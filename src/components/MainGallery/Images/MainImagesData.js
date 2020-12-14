be170adeded59519f75fccb3ef30190d7fe94e71
Weronika Chodanionek <weronika.chodanionek@jazzy.pro>
2020-12-15 00:37:14 +0100

    Revert "Removed unused images"
    This reverts commit 8c87196368206ff0f8f6c47f3f39495112304209.

---
+++ src/components/MainGallery/Images/MainImagesData.js
@@ -0,0 +1,100 @@
+import {
+  Portugalia1,
+  Portugalia2,
+  Portugalia3,
+  Portugalia4,
+  Portugalia5,
+  Portugalia6,
+  Portugalia7,
+  Portugalia8,
+  Portugalia9,
+  Portugalia10,
+  Portugalia11,
+  Portugalia12,
+  Portugalia13,
+  Portugalia14,
+  Portugalia15,
+  Portugalia16,
+  Portugalia17,
+  Portugalia18,
+  Portugalia19,
+  Portugalia20,
+  Portugalia21,
+  Portugalia22,
+  Portugalia23,
+} from "./index"
+
+const mainImagesPortugal = [
+  {
+    src: Portugalia1,
+  },
+  {
+    src: Portugalia2,
+  },
+  {
+    src: Portugalia3,
+  },
+  {
+    src: Portugalia4,
+  },
+
+  {
+    src: Portugalia5,
+  },
+  {
+    src: Portugalia23,
+  },
+  {
+    src: Portugalia6,
+  },
+  {
+    src: Portugalia7,
+  },
+  {
+    src: Portugalia8,
+  },
+  {
+    src: Portugalia9,
+  },
+  {
+    src: Portugalia10,
+  },
+  {
+    src: Portugalia11,
+  },
+  {
+    src: Portugalia12,
+  },
+  {
+    src: Portugalia13,
+  },
+  {
+    src: Portugalia14,
+  },
+  {
+    src: Portugalia15,
+  },
+  {
+    src: Portugalia16,
+  },
+  {
+    src: Portugalia17,
+  },
+  {
+    src: Portugalia18,
+  },
+  {
+    src: Portugalia19,
+  },
+  {
+    src: Portugalia20,
+  },
+  {
+    src: Portugalia21,
+  },
+  {
+    src: Portugalia22,
+  },
+];
+
+export default mainImagesPortugal;
