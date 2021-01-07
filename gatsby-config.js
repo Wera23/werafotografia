b29c9c174fb8feed819c25ca764bf2ed8eb330f2
Weronika Chodanionek <weronika.chodanionek@jazzy.pro>
2021-01-07 15:22:34 +0100

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,11 +52,27 @@ module.exports = {
         },
       },
     },
+  
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-analytics-gdpr`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-7XF9FQMLK2",
+        // The property ID; the tracking code won't be generated without it.
+        trackingId: "UA-82874229-1", 
+        // Optional parameter (default false) - Enable analytics in development mode.
+        enableDevelopment: true, // default false
+        // Optional parameter (default true) - Some countries (such as Germany) require you to use the _anonymizeIP function for Google Analytics. Otherwise you are not allowed to use it.
+        anonymizeIP: true,
+        head: true,
+        anonymize: true,
+        // Optional parameter (default false) - Starts google analytics with cookies enabled. In some countries (such as Germany) this is not allowed.
+        autoStartWithCookiesEnabled: false, 
+        // Optional parameter - Configuration for react-ga and google analytics 
+        reactGaOptions: {
+            debug: true,
+            gaOptions: {
+                sampleRate: 10
+            }
+        }
       },
     },
     {
